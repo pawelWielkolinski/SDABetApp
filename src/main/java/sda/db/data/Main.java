a1c54cf61a2aaa551f0a1da402950aff4231d96c
Paweł Wielkoliński <pawelw27@gmail.com>
2018-08-28 13:13:32 -0500

    get list of matches by date v1

--- src/main/java/sda/db/data/Main.java
+++ src/main/java/sda/db/data/Main.java
@@ -2,8 +2,10 @@ package sda.db.data;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseAccess databaseAccess = new DatabaseAccess();
 
-        System.out.println(databaseAccess.findAllMatches());
+        MatchRepository repository = new MatchRepository();
+
+        System.out.println(repository.getByDate("2017-07-04"));
+
     }
 }
--- src/main/java/sda/db/data/MatchRepository.java
+++ src/main/java/sda/db/data/MatchRepository.java
@@ -1,11 +1,13 @@
 package sda.db.data;
 
+import org.jooq.Result;
 import sda.db.data.generated.Tables;
 import sda.db.data.generated.tables.Matches;
 import sda.db.data.generated.tables.records.MatchesRecord;
 import org.jooq.DSLContext;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class MatchRepository extends DatabaseAccess {
@@ -38,6 +40,21 @@ public class MatchRepository extends DatabaseAccess {
             return ctx.selectFrom(match)
                     .where(match.ID_MATCH.eq(id))
                     .fetchOne();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Result<MatchesRecord> getByDate(String date) {
+
+        try (Connection conn = connection();) {
+            DSLContext ctx = jooq(conn);
+
+            Matches match = Tables.MATCHES;
+
+            return ctx.selectFrom(match)
+                    .where(match.START_DATE.eq(Date.valueOf(date)))
+                    .fetch();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
