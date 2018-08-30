67a860bd23b6f26b4d4068f5806c2f1b6ec91232
Paweł Wielkoliński <pawelw27@gmail.com>
2018-08-30 11:38:15 -0500

    fixing matchDate model

--- src/main/java/sda/match/MatchController.java
+++ src/main/java/sda/match/MatchController.java
@@ -17,7 +17,7 @@ public class MatchController {
 
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("MatchDate", new MatchDate());
+        model.addAttribute("matchDate", new MatchDate());
         return "index";
     }
 
