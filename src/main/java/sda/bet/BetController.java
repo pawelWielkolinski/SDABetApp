8dd790cde1b1d9f3b8230249373350281ce2d2d6
Paweł Wielkoliński <pawelw27@gmail.com>
2018-09-03 04:38:24 -0500

    your bets without match info

--- src/main/java/sda/bet/BetController.java
+++ src/main/java/sda/bet/BetController.java
@@ -6,19 +6,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sda.db.data.generated.tables.records.BetsRecord;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class BetController {
 
     @Autowired
     private BetService betService;
+    @Autowired
+    private HttpSession session;
 
     @PostMapping ("/setBet")
     public String setBet(@ModelAttribute BetForm betInfo, Model model) {
 
         betService.saveBet(betInfo);
 
-        return "matches";
+        return "betSuccessful";
+    }
+
+    @RequestMapping("/bets")
+    public String bets(Model model) {
+        BetRepository betRepository = new BetRepository();
+
+        List<BetsRecord> yourBets = betRepository.getBetsByUserId((Integer) session.getAttribute("idUser"));
+
+        model.addAttribute("yourBets", yourBets);
+
+        return "bets";
     }
 
 
