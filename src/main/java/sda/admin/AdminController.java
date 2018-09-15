204c8e6e104670692c8a0a59b2572f2f7d4f6944
Paweł Wielkoliński <pawelw27@gmail.com>
2018-09-15 07:13:13 -0500

    working update match database in admin panel

--- src/main/java/sda/admin/AdminController.java
+++ src/main/java/sda/admin/AdminController.java
@@ -4,7 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import sda.db.data.generated.tables.records.UsersRecord;
+import sda.match.MatchDate;
+import sda.match.model.Matches;
+import sda.match.model.MatchesModel;
+import sda.user.UserRepository;
 import sda.user.UserService;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class AdminController {
@@ -12,13 +22,33 @@ public class AdminController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/adminpanel")
-    public String update(Model model){
+    @Autowired
+    UserRepository userRepository;
 
+    @GetMapping("/panel")
+    public String admin(Model model) {
+        List<UsersRecord> users = userRepository.getAll();
+        model.addAttribute("UsersList", users);
+        model.addAttribute("MatchDate", new MatchDate());
         return "panel";
     }
 
+    @PostMapping("/updateMatchDatabase")
+    public String updateMatches(@ModelAttribute MatchDate matchDate, Model model) throws IOException {
 
+        MatchesModel matchesModel = new MatchesModel();
+        Matches matchesFromApi = matchesModel.getMatchesFromApi(matchDate.getDateFrom(), matchDate.getDateTo());
+        matchesModel.insertMatchesToDatabase(matchesFromApi);
 
-
+        return "redirect:panel";
     }
+
+    @PostMapping("/manageUser")
+    public String manage(Model model) {
+
+
+        return "redirect:panel";
+    }
+
+
+}
